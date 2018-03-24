f2407c3aa29b9c22798cd3a7edd1b2d9863b2861
realneax <32615324+realneax@users.noreply.github.com>
2018-03-24 10:16:48 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,132 @@
+const Discord = require("discord.js");
+
+
+const client = new Discord.Client({
+    owner: '285887620813160450'
+});
+
+
+const config = require("./config.json");
+
+client.on("ready", () => {
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+client.on("guildCreate", guild => {
+
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setGame(`on ${client.guilds.size} servers`);
+});
+
+
+client.on("message", async message => {
+  
+  if(message.author.bot) return;
+  
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  
+  if(command === "ping") {
+ 
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+  
+  if(command === "e") {
+    if(!message.member.roles.some(r=>["Assistant", "Owners", "Leaders", "Developers", "Manager"].includes(r.name)) )
+    return message.reply("Sorry, you don't have permissions to use this!");
+    const sayMessage = args.join(" ");
+
+    message.delete().catch(O_o=>{}); 
+
+    message.channel.send({embed:{
+      color: 0x000000,
+      description: sayMessage
+    }})
+  }
+  
+  if(command === "kick") {
+    if(!message.member.roles.some(r=>["Admin", "Nadeko", "The_GodFather", "Team Leader"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.kickable) 
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the kick!");
+
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.channel.send({embed:{
+        color: 0x00f469,
+        description: `${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`
+    }});
+
+  }
+
+  if(message.content.startsWith(config.prefix + "prune")){
+    if(!message.member.roles.some(r=>["Admin", "Team Leader"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    let args = message.content.split(" ").slice(1);
+    let author = message.member;
+    let role = message.guild.roles.find('name', "MFA");
+    if(author.roles.has(role.id)){
+        if(!args[0]){
+            message.delete();
+            message.author.send("No args gives.");
+            return;
+        }
+        if(args[0] > 100){
+            message.delete();
+            message.author.send("Not more then 100.");
+            return;
+        }
+
+        message.delete();
+        message.channel.bulkDelete(args[0]);
+        message.channel.send("Succesfully deleted " + args[0] + " messages.")
+        return;
+      }
+    }
+  
+  if(command === "ban") {
+ 
+    if(!message.member.roles.some(r=>["Admin", "Nadeko", "perms"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.bannable) 
+      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the ban!");
+    
+    await member.ban(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    message.channel.send({embed:{
+        color: 0x00f469,
+        description: `${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`
+    }});
+  }
+
+});
+
+client.login(process.env.BOT_TOKEN);
