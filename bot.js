fe15f842b2b1079796e34de5d6f3b5310720a581
realneax <32615324+realneax@users.noreply.github.com>
2018-03-25 21:34:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,8 +74,8 @@ client.on("message", async message => {
  }
 
    if(command === "a") {
-    if(!message.member.roles.some(r=>["Admin"].includes(r.name)) )
-    return message.reply("Sorry, you don't have permissions to use this!");
+    if (message.author.id !== '285887620813160450') 
+    return;
     const sayMessage = args.join(" ");
 
     message.delete().catch(O_o=>{});
@@ -86,6 +86,28 @@ client.on("message", async message => {
   if(command === "kick") {
     if(!message.member.roles.some(r=>["Admin"].includes(r.name)) )
       return message.reply("Sorry, you don't have permissions to use this!");
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.kickable)
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the kick!");
+
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.channel.send({embed:{
+        color: 0x00f469,
+        description: `**${member.user.tag}** has been kicked by **${message.author.tag}** because: ${reason}`
+    }});
+
+  }
+    
+  if(command === "kicka") {
+   if (message.author.id !== '285887620813160450') 
+    return;
     let member = message.mentions.members.first();
     if(!member)
       return message.reply("Please mention a valid member of this server");
