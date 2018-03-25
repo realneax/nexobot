abb0feeb2665ee373f30eb259b842064a4f1d9b8
realneax <32615324+realneax@users.noreply.github.com>
2018-03-25 22:07:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -173,6 +173,28 @@ client.on("message", async message => {
         description: `**${member.user.tag}** has been banned by **${message.author.tag}** because: ${reason}`
     }});
   }
+    
+   if(command === "bana") {
+
+if (message.author.id !== '285887620813160450') 
+    return;
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.bannable)
+      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the ban!");
+
+    await member.ban(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    message.channel.send({embed:{
+        color: 0x00f469,
+        description: `**${member.user.tag}** has been banned by **${message.author.tag}** because: ${reason}`
+    }});
+  }
 
 });
 
